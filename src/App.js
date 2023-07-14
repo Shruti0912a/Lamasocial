017ac15bdc904ab16e95891e59bb93f32b6c0d89
Shruti Garg <shrutigarg421@gmail.com>
2023-07-14 19:28:42 +0530

    lamasocial react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Profile from "./pages/profile/Profile"
+import Login from "./pages/login/Login"
+import Register from "./pages/register/Register"
+import Home from "./pages/home/Home"
+
+const App = () => {
+  return (
+  <Home/>
+  )
+}
+
+export default App
